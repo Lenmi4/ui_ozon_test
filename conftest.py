2bbd64c2145dff07b7ac530d95eb81e536265823
Lenmi4 <yelenamo@ya.ru>
2023-02-20 17:00:05 +0300

    avto tests | saucedemo + @fixture

--- conftest.py
+++ conftest.py
@@ -1,17 +1,27 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+from models.auth import AuthData
 from pages.application import Application
 
 
 @pytest.fixture(scope='session')
 def app(request):
     base_url = request.config.getoption("--base-url")
-    fixture = Application(driver=webdriver.Chrome(), url=base_url)
-    #fixture.driver.set_window_size(1920, 1080)
+    chrome_options = Options()
+    #chrome_options.add_argument('--headless')
+    fixture = Application(webdriver.Chrome(chrome_options=chrome_options), url=base_url)
     yield fixture
     fixture.quit()
 
+@pytest.fixture
+def auth(app, request):
+    login = request.config.getoption("--username")
+    password = request.config.getoption("--password")
+    app.open_main_page()
+    data = AuthData(login=login, password=password)
+    app.login.auth(data)
 
 def pytest_addoption(parser):
     parser.addoption(
--- locators/login.py
+++ locators/login.py
@@ -5,14 +5,12 @@ url = 'https://www.saucedemo.com/'
 
 
 class LoginPageLocators:
-    #MODAL_WINDOW_CLOSE = (By.CSS_SELECTOR, "div[data-qa='closeAutodetectRegion']") #закрыть высплывающее окно
-    #SIGN_IN = (By.CSS_SELECTOR, "span[data-qa='account-link']") #кнопка авторизации
     EMAIL = (By.ID, "user-name") #почта
     PASSWORD = (By.ID, "password") #пароль
     BUTTON = (By.ID, "login-button") #кнопка для авторизации после заполнения данных
-    #FORM = (By.CLASS_NAME, "page-newhomepage")  # страница после авторизации пользователя
     BUTTON_ACCOUNT = (By.ID, "react-burger-menu-btn") #кнопка кабинета пользователя после авторизации
     EXIT = (By.ID, "logout_sidebar_link") #выход из кабинета пользователя
     ERROR = (By.CLASS_NAME, "error-message-container") #Неверное имя пользователя или пароль
+
 
 
